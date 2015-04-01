c1fecf03354882d08860707dc4bee3ddab8615b0
John Beynon <johnb@heroku.com>
2015-04-01 17:28:10 +0100

    Add collection selector to navbar and change to using icons

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,5 +1,6 @@
 class ResourcesController < ApplicationController
   def index
+    @collections = @api.collection.list
     @resources = @api.resource.lists(ENV["STASH_COLLECTION_ID"])
     @tags = @api.tag.lists(ENV["STASH_COLLECTION_ID"])
   end

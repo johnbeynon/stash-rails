8ab3db38a08589b7e67533e9092f5f6292f63a96
John Beynon <john@beynon.org.uk>
2015-04-04 21:42:51 +0100

    add ajaxsetup

---
+++ app/assets/javascripts/stash.js
@@ -0,0 +1,8 @@
+$.ajaxSetup({
+  headers: {
+    'Authorization': 'Token token=foobah',
+    'Accept': 'application/vnd.stash+json; version=1'
+  },
+  contentType: "application/json; charset=utf-8",
+  dataType: "json",
+});
